6ce59fb1dd29191781c42346a218ca13f5fba04c
Emilio Gallicchio <emilio.gallicchio@gmail.com>
2023-01-02 11:50:45 -0500

    set jobname

--- examples/RBFE/cdk2/scripts/run_template.sh
+++ examples/RBFE/cdk2/scripts/run_template.sh
@@ -10,12 +10,14 @@
 #SBATCH --no-requeue
 #SBATCH -t 10:00:00
 
+jobname=<JOBNAME>
+
 . ~/miniconda3/bin/activate atm
 echo "Running on $(hostname)"
 
-if [ ! -f <JOBNAME>_0.xml ]; then
+if [ ! -f ${jobname}_0.xml ]; then
    python <ASYNCRE_DIR>/rbfe_structprep.py ${jobname}_asyncre.cntl || exit 1
 fi
 
 echo "localhost,0:0,1,CUDA,,/tmp" > nodefile
-python <ASYNCRE_DIR>/rbfe_explicit.py <JOBNAME>_asyncre.cntl
+python <ASYNCRE_DIR>/rbfe_explicit.py ${jobname}_asyncre.cntl

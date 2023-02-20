0f9035ce6fea656e26e612aa85c638385acdd987
Raimondas Galvelis <r.galvelis@acellera.com>
2023-02-20 18:47:53 +0100

    Revive restarting

--- openmm_sync_re.py
+++ openmm_sync_re.py
@@ -84,22 +84,28 @@ class sync_re:
 
     def scheduleJobs(self):
 
-        max_samples = None
-        assert self.config.get('MAX_SAMPLES'), "MAX_SAMPLES has to be specified"
-        max_samples = int(self.config.get('MAX_SAMPLES'))
+        assert 'MAX_SAMPLES' in self.config, "MAX_SAMPLES has to be specified"
+        num_samples = int(self.config['MAX_SAMPLES'])
 
-        # TODO: restart properly
-        num_sims = max_samples * len(self.openmm_replicas)
-        for isample in range(max_samples):
+        last_sample = self.openmm_replicas[0].get_cycle() - 1
+        for isample in range(last_sample, num_samples):
             for irepl, replica in enumerate(self.openmm_replicas):
                 self.logger.info(f"Simulation: sample {isample}, replica {irepl}")
+
+                assert replica.get_cycle() == self.status[irepl]['cycle_current']
+                assert replica.get_cycle() in (isample, isample + 1)
+                if replica.get_cycle() == isample + 1:
+                    self.logger.debug(f"Replica {irepl} already has {replica.get_cycle()} samples")
+                    continue
+
                 self._launchReplica(irepl, self.status[irepl]['cycle_current'])
                 self.status[irepl]['cycle_current'] += 1
                 assert replica.get_cycle() == self.status[irepl]['cycle_current']
+
                 self.doExchanges()
                 self.updateStatus()
-                self.checkpointJob()
-                self.print_status()
+
+            self.checkpointJob()
 
     def updateStatus(self):
         """Scan the replicas and update their states."""

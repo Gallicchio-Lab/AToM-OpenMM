b19a6b184f19250d16df2f2418a473858f26d58e
Raimondas Galvelis <r.galvelis@acellera.com>
2023-02-21 13:58:32 +0100

    Clean up utils.timer

--- utils/timer.py
+++ utils/timer.py
@@ -1,21 +1,6 @@
 from contextlib import contextmanager
 import time
 
-# class Timer2:
-
-#     def __init__(self, logger, message):
-#         self.logger = logger
-#         self.message = message
-
-#     def __enter__(self):
-#         self.start = time.monotonic()
-#         self.logger(f"Started: {self.message}")
-#         return None
-
-#     def __exit__(self, exc_type, exc_value, exc_tb):
-#         duration = time.monotonic() - self.start
-#         self.logger(f"Finished: {self.message} (duration: {duration} s)")
-
 @contextmanager
 def Timer(logger, message):
     logger(f"Started: {message}")

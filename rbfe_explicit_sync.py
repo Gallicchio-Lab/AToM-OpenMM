a8c55e3dd47dc5c8495921ff05a3eb96aeda0d76
Raimondas Galvelis <r.galvelis@acellera.com>
2023-03-01 12:24:53 +0100

    Clean up the sync code

--- rbfe_explicit_sync.py
+++ rbfe_explicit_sync.py
@@ -1,29 +1,9 @@
 import sys
-import time
-
 from sync.atm import openmm_job_AmberRBFE
 
 if __name__ == '__main__':
+    assert len(sys.argv) == 2, "Specify ONE input file"
 
-    # Parse arguments:
-    usage = "%prog <ConfigFile>"
-
-    if len(sys.argv) != 2:
-        print("Please specify ONE input file")
-        sys.exit(1)
-
-    config_file = sys.argv[1]
-
-    print("")
-    print("=======================================")
-    print("AToM RBFE Synchronous Replica Exchange ")
-    print("=======================================")
-    print("")
-    print("Started at: " + str(time.asctime()))
-    print("Input file:", config_file)
-    print("")
-    sys.stdout.flush()
-
-    rx = openmm_job_AmberRBFE(config_file)
+    rx = openmm_job_AmberRBFE(sys.argv[1])
     rx.setupJob()
     rx.scheduleJobs()

66070168f24e0f88bbe377e14c9067dde7ccbbd5
sheenam1509 <sheenam.1509@gmail.com>
2023-04-07 23:14:12 -0400

    Revised Setup File for ATM-Setup

--- examples/ABFE/fkbp/scripts/setup-atm.sh
+++ examples/ABFE/fkbp/scripts/setup-atm.sh
@@ -7,75 +7,23 @@ source ${scripts_dir}/setup-settings.sh
 
 cd ${work_dir} || exit 1
 
-#convert list of resids of receptor into Calpha atom indexes
-#these atoms are restrained.
-#Indexes are shifted by 1 as they are expected to start from 0 in the OpenMM scripts
-calpha_atoms=$( awk '/^ATOM/ && $3 ~ /^CA$/ {print $2}' receptor/${receptor}.pdb ) || exit 1
-for i in $calpha_atoms; do
-    i1=$( expr $i - 1 )
-    if [ -z "$restr_atoms" ] ; then
-	restr_atoms=$i1
-    else
-	restr_atoms="${restr_atoms}, $i1"
-    fi
-done
-echo "Indexes of the Calpha atoms:"
-echo "$restr_atoms"
-
-#get the list of receptor atoms that define the centroid of the binding site
-calphascm=()
-n=0
-for res in ${vsite_rcpt_residues[@]}; do 
-    i=$( awk -v resid=$res '/^ATOM/ && $5 == resid && $3 ~ /^CA$/ {print $2}' receptor/${receptor}.pdb ) || exit 1
-    calphascm[$n]=$i
-    n=$(expr $n + 1)
-done
-for i in ${calphascm[@]}; do
-    i1=$( expr $i - 1 )
-    if [ -z "$vsite_rcpt_atoms" ] ; then
-	vsite_rcpt_atoms=$i1
-    else
-	vsite_rcpt_atoms="${vsite_rcpt_atoms}, $i1"
-    fi
-done
-echo "Vsite receptor atoms:"
-echo "$vsite_rcpt_atoms"
-
 #process each ligand
 nligands=${#ligands[@]}
 nlig_m1=$(expr ${#ligands[@]} - 1)
 
 cd ${work_dir} || exit 1
 for l in `seq 0 ${nlig_m1}` ; do
-    
-    lig=${ligands[$l]}
-    
-    echo "Processing ligand  $lig..."
-    
-    #retrieve number of atoms of the receptor from the pdb file
-    #and those of the ligands from their mol2 files
-    num_atoms_rcpt=$( awk '/^ATOM /{print $0}' ${work_dir}/receptor/${receptor}.pdb | wc -l ) || exit 1
-    num_atoms_lig=$( awk 'NR==3{print $1}' ${work_dir}/ligands/${lig}.mol2 ) || exit 1
 
-    #list of ligand atoms (starting from zero) assuming they are listed soon after the receptor
-    n1=$( expr $num_atoms_rcpt + $num_atoms_lig - 1 )
-    lig_atoms=""
-    for i in $( seq $num_atoms_rcpt $n1 ); do
-	if [ -z "$lig_atoms" ] ; then
-	    lig_atoms=$i
-	else
-	    lig_atoms="${lig_atoms}, $i"
-	fi
-    done
-    
-    echo "atoms of $lig are $lig_atoms"
-    
+    lig=${ligands[$l]}
+
+    echo "Processing ligand  $lig..."
+
 
     #assign GAFF2 parameters to ligands
     cd ${work_dir}/ligands || exit 1
     
     if [ ! -f ${lig}-p.mol2 ] ; then
-	charge=$( awk 'BEGIN{charge = 0} ; NF == 9 {charge += $9} ; END {if (charge >= 0) {print int(charge + 0.5)} else {print int(charge - 0.5)}}' < ${lig}.mol2 ) || exit 1
+	charge=$( awk 'BEGIN{charge = 0} ; NF == 9 {charge += $9} ; END {if (charge >= 0) {print int(charge + 0.5)} else {print int(charge -0.5)}}' < ${lig}.mol2 ) || exit 1
 	echo "charge: $charge"
 	echo "antechamber -pl 15 -fi mol2 -fo mol2 -i ${lig}.mol2 -o ${lig}-p.mol2 -c bcc -nc ${charge} -at gaff2"
 	antechamber -pl 15 -fi mol2 -fo mol2 -i ${lig}.mol2 -o ${lig}-p.mol2 -c bcc -nc ${charge} -at gaff2 || exit 1
@@ -99,9 +47,9 @@ for l in `seq 0 ${nlig_m1}` ; do
 source leaprc.protein.ff14SB
 source leaprc.gaff2
 source leaprc.water.tip3p
-RCPT = loadpdb "${rcptpdb}"
-LIG = loadmol2 "${ligmol2}"
-loadamberparams "${ligfrcmod}"
+RCPT = loadpdb "${work_dir}/receptor/${receptor}.pdb"
+LIG = loadmol2 "${work_dir}/ligands/${lig}-p.mol2"
+loadamberparams "${work_dir}/ligands/${lig}-p.frcmod"
 MOL = combine {RCPT LIG}
 addions2 MOL Na+ 0
 addions2 MOL Cl- 0
@@ -110,8 +58,69 @@ saveamberparm MOL ${jobname}.prmtop ${jobname}.inpcrd
 savepdb MOL ${jobname}.pdb
 quit
 EOF
-    echo "tleap -f tleap.cmd"
-    tleap -f tleap.cmd || exit 1
+    if [ ! -f ${jobname}.pdb ] ; then
+	tleap -f tleap.cmd || exit 1
+    
+    fi
+    #convert list of resids of receptor into Calpha atom indexes
+    #these atoms are restrained.
+    #Indexes are shifted by 1 as they are expected to start from 0 in the OpenMM scripts
+    calpha_atoms=$( awk '/^ATOM/ && $3 ~ /^CA$/ {print $2}' ${jobname}.pdb ) || exit 1
+    unset restr_atoms
+    for i in $calpha_atoms; do
+	i1=$( expr $i - 1 )
+	if [ -z "$restr_atoms" ] ; then
+	    restr_atoms=$i1
+	else
+	    restr_atoms="${restr_atoms}, $i1"
+	fi
+    done
+    echo "Indexes of the Calpha atoms:"
+    echo "$restr_atoms"
+
+    #get the list of receptor atoms that define the centroid of the binding site
+    calphascm=()
+    n=0
+    for res in ${vsite_rcpt_residues[@]}; do 
+	i=$( awk -v resid=$res '/^ATOM/ && $5 == resid && $3 ~ /^CA$/ {print $2}' ${jobname}.pdb ) || exit 1
+	calphascm[$n]=$i
+	n=$(expr $n + 1)
+    done
+    unset vsite_rcpt_atoms
+    for i in ${calphascm[@]}; do
+	i1=$( expr $i - 1 )
+	if [ -z "$vsite_rcpt_atoms" ] ; then
+	    vsite_rcpt_atoms=$i1
+	else
+	    vsite_rcpt_atoms="${vsite_rcpt_atoms}, $i1"
+	fi
+    done
+    echo "Vsite receptor atoms:"
+    echo "$vsite_rcpt_atoms"
+
+#search for the first ligand to find the size of the receptor
+    namereslig1=$( awk  'f{printf("%.3s", $8);f=0} /@<TRIPOS>ATOM/{f=1}' ${work_dir}/ligands/${lig}.mol2 ) || exit 1
+    l1=$( awk "\$4 ~ /${namereslig1}/{print \$2}" < ${jobname}.pdb  | head -1 ) || exit 1
+    lig1start=$(expr $l1 - 1 )
+    num_atoms_rcpt=$(expr $l1 - 1 )
+    #retrieve number of atoms of the ligands from their mol2 files
+    num_atoms_lig1=$( awk 'NR==3{print $1}' ${work_dir}/ligands/${lig}.mol2 ) || exit 1
+    
+    
+    #list of ligand atoms (starting from zero) assuming they are listed soon after the receptor
+    lig1end=$( expr $lig1start + $num_atoms_lig1 - 1 )
+    lig1_atoms=""
+    for i in $( seq $lig1start $lig1end ); do
+	if [ -z "$lig1_atoms" ] ; then
+	    lig1_atoms=$i
+	else
+	    lig1_atoms="${lig1_atoms}, $i"
+	fi
+    done
+    
+    echo "atoms of $lig are $lig1_atoms"
+    
+   
     
     displs=""
     negative_displ=()
@@ -123,7 +132,7 @@ EOF
     echo "Displacement vector: $displs"
 
     #builds mintherm, npt, and equilibration scripts
-    replstring="s#<JOBNAME>#${jobname}# ; s#<DISPLX>#${displacement[0]}# ; s#<DISPLY>#${displacement[1]}# ; s#<DISPLZ>#${displacement[2]}# ; s#<VSITERECEPTORATOMS>#${vsite_rcpt_atoms}# ; s#<RESTRAINEDATOMS>#${restr_atoms}# ; s#<LIGATOMS>#${lig_atoms}#" 
+    replstring="s#<JOBNAME>#${jobname}# ; s#<DISPLX>#${displacement[0]}# ; s#<DISPLY>#${displacement[1]}# ; s#<DISPLZ>#${displacement[2]}# ; s#<VSITERECEPTORATOMS>#${vsite_rcpt_atoms}# ; s#<RESTRAINEDATOMS>#${restr_atoms}# ; s#<LIGATOMS>#${lig1_atoms}#" 
     sed "${replstring}" < ${work_dir}/scripts/mintherm_template.py > ${jobname}_mintherm.py || exit 1
     sed "${replstring}" < ${work_dir}/scripts/equil_template.py > ${jobname}_equil.py || exit 1
     sed "${replstring}" < ${work_dir}/scripts/mdlambda_template.py > ${jobname}_mdlambda.py || exit 1

dfcaf5d38228d8fbb0ab072f71f9d4bf1a3922cb
Stefan Doerr <stefdoerr@gmail.com>
2025-05-22 11:51:04 +0300

    added uwham tests

---
+++ tests/test_uwham.py
@@ -0,0 +1,41 @@
+import os
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+def _test_uwham_analysis(tmp_path):
+    from atom_openmm.uwham import calculate_uwham
+
+    run_dir = os.path.join(curr_dir, "TYK2_A02_A09", "TYK2_A02_A09_r0_1")
+    ddG, ddG_std, dgbind1, dgbind2, samples = calculate_uwham(run_dir, "QB_A02_A09", 70)
+
+    expected_ddG = -1.0582613105156682
+    expected_ddG_std = 0.1967262793670161
+    assert abs(ddG - expected_ddG) < 1e-8
+    assert abs(ddG_std - expected_ddG_std) < 1e-8
+    assert samples == 351
+    assert abs(dgbind1 - 16.12549393024899) < 1e-8
+    assert abs(dgbind2 - 17.183755240764658) < 1e-8
+
+    run_dir = os.path.join(curr_dir, "TYK2_A02_A09", "TYK2_A02_A09_r0_2")
+    ddG, ddG_std, dgbind1, dgbind2, samples = calculate_uwham(run_dir, "QB_A02_A09", 70)
+
+    expected_ddG = -1.9492444343451716
+    expected_ddG_std = 0.18548486855089807
+    assert abs(ddG - expected_ddG) < 1e-8
+    assert abs(ddG_std - expected_ddG_std) < 1e-8
+    assert samples == 421
+    assert abs(dgbind1 - 15.690178371190054) < 1e-8
+    assert abs(dgbind2 - 17.639422805535226) < 1e-8
+
+    run_dir = os.path.join(curr_dir, "TYK2_A02_A09", "TYK2_A02_A09_r0_3")
+    ddG, ddG_std, dgbind1, dgbind2, samples = calculate_uwham(
+        run_dir, "QB_A02_A09", None
+    )
+
+    expected_ddG = -1.2625416487680994
+    expected_ddG_std = 0.2068400471106031
+    assert abs(ddG - expected_ddG) < 1e-8
+    assert abs(ddG_std - expected_ddG_std) < 1e-8
+    assert samples == 480
+    assert abs(dgbind1 - 16.159606073746524) < 1e-8
+    assert abs(dgbind2 - 17.422147722514623) < 1e-8

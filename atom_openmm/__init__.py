f136c45a9bdb90fa641cde97cd89439d1b7d54b2
Stefan Doerr <stefdoerr@gmail.com>
2025-06-04 13:17:25 +0300

    fixed logging setup

--- atom_openmm/__init__.py
+++ atom_openmm/__init__.py
@@ -1,5 +1,5 @@
 from importlib.metadata import version, PackageNotFoundError
-import logging
+import logging.config
 import os
 
 __curr_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +12,8 @@ except PackageNotFoundError:
 
 try:
     logging.config.fileConfig(
-        os.path.join(__curr_dir, "utils", "logging.conf"), disable_existing_loggers=False
+        os.path.join(__curr_dir, "utils", "logging.conf"),
+        disable_existing_loggers=False,
     )
-except Exception:
-    print("AToM-OpenMM: Logging setup failed")
+except Exception as e:
+    print(f"AToM-OpenMM: Logging setup failed: {e}")

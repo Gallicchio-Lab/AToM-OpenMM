8e6ddc4214d6661eb64888289ceb6924ddee6cfd
Stefan Doerr <stefdoerr@gmail.com>
2025-07-15 14:35:52 +0200

    cleanup tests

--- tests/test_atm.py
+++ tests/test_atm.py
@@ -17,11 +17,9 @@ def _test_abfe_structprep(tmp_path):
 def _test_abfe_production(tmp_path):
     from atom_openmm.abfe_production import abfe_production
 
-    shutil.copytree(
-        os.path.join(curr_dir, "1STP_equil"),
-        os.path.join(tmp_path, "1STP_equil"),
-    )
     run_dir = os.path.join(tmp_path, "1STP_equil")
+    shutil.copytree(os.path.join(curr_dir, "1STP_equil"), run_dir)
+
     abfe_production(os.path.join(run_dir, "1STP_asyncre.yaml"))
 
 
@@ -61,11 +59,9 @@ def _test_rbfe_structprep(tmp_path):
 def _test_rbfe_production(tmp_path):
     from atom_openmm.rbfe_production import rbfe_production
 
-    shutil.copytree(
-        os.path.join(curr_dir, "QB_A08_A07_equil"),
-        os.path.join(tmp_path, "QB_A08_A07_equil"),
-    )
     run_dir = os.path.join(tmp_path, "QB_A08_A07_equil")
+    shutil.copytree(os.path.join(curr_dir, "QB_A08_A07_equil"), run_dir)
+
     rbfe_production(os.path.join(run_dir, "QB_A08_A07_asyncre.yaml"))
 
 
@@ -183,43 +179,26 @@ def _test_input_parser():
 
 
 def _test_sync_production_ats(tmp_path):
-    from atom_openmm.openmm_async_re import openmm_job_RBFE
-
-    shutil.copytree(
-        os.path.join(curr_dir, "QB_A08_A07_equil_sync_ats"),
-        os.path.join(tmp_path, "QB_A08_A07_equil_sync_ats"),
-    )
+    from atom_openmm.rbfe_production import rbfe_production
 
     run_dir = os.path.join(tmp_path, "QB_A08_A07_equil_sync_ats")
-    os.chdir(run_dir)
+    shutil.copytree(os.path.join(curr_dir, "QB_A08_A07_equil_sync_ats"), run_dir)
 
-    rx = openmm_job_RBFE("QB_A08_A07_asyncre.yaml", options=None)
-    rx.setupJob()
-    rx.scheduleJobs()
+    rbfe_production(os.path.join(run_dir, "QB_A08_A07_asyncre.yaml"))
+
     for i in range(4):
-        assert os.path.exists(
-            os.path.join(
-                tmp_path, "QB_A08_A07_equil_sync_ats", f"r{i}", "QB_A08_A07.xtc"
-            )
-        )
+        assert os.path.exists(os.path.join(run_dir, f"r{i}", "QB_A08_A07.xtc"))
 
 
 def _test_sync_production_incremental(tmp_path):
-    from atom_openmm.openmm_async_re import openmm_job_RBFE
+    from atom_openmm.rbfe_production import rbfe_production
     from openmm.app.internal.xtc_utils import read_xtc
     import yaml
 
-    def rbfe_production(configfile):
-        rx = openmm_job_RBFE(configfile, options=None)
-        rx.setupJob()
-        rx.scheduleJobs()
+    run_dir = os.path.join(tmp_path, "QB_A08_A07_equil_sync")
+    shutil.copytree(os.path.join(curr_dir, "QB_A08_A07_equil_sync"), run_dir)
 
-    exec_dir = os.path.join(tmp_path, "QB_A08_A07_equil_sync")
-    shutil.copytree(os.path.join(curr_dir, "QB_A08_A07_equil_sync"), exec_dir)
-
-    os.chdir(exec_dir)
-
-    configfile = os.path.join(exec_dir, "QB_A08_A07_asyncre.yaml")
+    configfile = os.path.join(run_dir, "QB_A08_A07_asyncre.yaml")
     with open(configfile, "r") as f:
         config = yaml.safe_load(f)
     config["MAX_SAMPLES"] = "+1"
@@ -228,18 +207,18 @@ def _test_sync_production_incremental(tmp_path):
 
     rbfe_production(configfile)
     for i in range(4):
-        xtcf = os.path.join(exec_dir, f"r{i}", "QB_A08_A07.xtc")
+        xtcf = os.path.join(run_dir, f"r{i}", "QB_A08_A07.xtc")
         coords_read, box_read, time, step = read_xtc(xtcf.encode("utf-8"))
         assert coords_read.shape == (46380, 3, 1)
         assert box_read.shape == (3, 3, 1)
         assert len(time) == 1
         assert len(step) == 1
 
-    startsampl_file = os.path.join(exec_dir, "starting_sample")
+    startsampl_file = os.path.join(run_dir, "starting_sample")
     with open(startsampl_file, "r") as f:
         starting_sample = int(f.read().strip())
         assert starting_sample == 0
-    prog_file = os.path.join(exec_dir, "progress")
+    prog_file = os.path.join(run_dir, "progress")
     with open(prog_file, "r") as f:
         progress = float(f.read().strip())
         assert progress == 0.0
@@ -250,7 +229,7 @@ def _test_sync_production_incremental(tmp_path):
         yaml.dump(config, f)
     rbfe_production(configfile)
     for i in range(4):
-        xtcf = os.path.join(exec_dir, f"r{i}", "QB_A08_A07.xtc")
+        xtcf = os.path.join(run_dir, f"r{i}", "QB_A08_A07.xtc")
         coords_read, box_read, time, step = read_xtc(xtcf.encode("utf-8"))
         assert coords_read.shape == (46380, 3, 2)
         assert box_read.shape == (3, 3, 2)
@@ -272,7 +251,7 @@ def _test_sync_production_incremental(tmp_path):
         yaml.dump(config, f)
     rbfe_production(configfile)
     for i in range(4):
-        xtcf = os.path.join(exec_dir, f"r{i}", "QB_A08_A07.xtc")
+        xtcf = os.path.join(run_dir, f"r{i}", "QB_A08_A07.xtc")
         coords_read, box_read, time, step = read_xtc(xtcf.encode("utf-8"))
         assert coords_read.shape == (46380, 3, 4)
         assert box_read.shape == (3, 3, 4)
